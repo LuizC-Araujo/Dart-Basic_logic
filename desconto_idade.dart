23d8d3017e7885efd20daf1767f866c34452adb1
LuizC-Araujo <arauldesign@gmail.com>
2020-09-03 20:41:21 -0300

    desconto para idade par

---
+++ desconto_idade.dart
@@ -0,0 +1,15 @@
+/*Dar descontos somente se a pessoa tiver idade ímpar.
+author: luiz araujo
+date: 03/09/2020
+ */
+
+void main() {
+  num idade = 21;
+  double valor = 950.00, desconto = 0.20;
+
+  if (idade % 2 != 0) {
+    valor = valor - desconto * valor;
+    print("O valor com desconto é $valor");
+  } else
+    print("Sua idade não é ímpar, não tem desconto.");
+}
