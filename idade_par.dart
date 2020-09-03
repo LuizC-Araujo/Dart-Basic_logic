5be1fc288507bd7eca15591e74a6184aa07016c5
LuizC-Araujo <arauldesign@gmail.com>
2020-09-03 20:31:37 -0300

    verifica se a idade é par

---
+++ idade_par.dart
@@ -0,0 +1,12 @@
+/*Desenvolver um programa em dart para descobrir se uma pessoa tem idade par.
+author: luiz araujo
+date: 03/09/2020
+ */
+void main() {
+  num idade = 21;
+
+  if (idade % 2 == 0)
+    print("A Idade é par");
+  else
+    print("A idade não é par");
+}
