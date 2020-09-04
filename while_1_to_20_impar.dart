da2827fc34a540c0c99d7c9d3fba7d287c5b9a74
LuizC-Araujo <arauldesign@gmail.com>
2020-09-03 21:22:28 -0300

    while show impar

---
+++ while_1_to_20_impar.dart
@@ -0,0 +1,20 @@
+/*Utilizando o while faça um programa em dart que imprima somente 
+números ímpares de 1 até 20.
+author: luiz araujo
+date: 03/09/2020
+ */
+void main() {
+  int num = 1;
+  while (num < 21) {
+    print(num);
+    num += 2;
+  }
+}
+
+// void main() {
+//   int num = 1;
+//   while (num < 21) {
+//     if (num % 2 != 0) print(num);
+//     num++;
+//   }
+// }
