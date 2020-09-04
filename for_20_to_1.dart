bc65f67d1056b2b0c4f7c111cd5d98a2529e3ee9
LuizC-Araujo <arauldesign@gmail.com>
2020-09-03 21:15:12 -0300

    for 20 to 1

---
+++ for_20_to_1.dart
@@ -0,0 +1,10 @@
+/*Faça um programa usando o for que imprima os números de 20 até 1.
+author: luiz araujo
+date: 03/09/2020
+ */
+void main() {
+  int num;
+  for (num = 20; num > 0; num--) {
+    print(num);
+  }
+}
