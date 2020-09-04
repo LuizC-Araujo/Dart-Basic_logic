c08314e29d30b7efb80030e4845094d20d2b1bbc
LuizC-Araujo <arauldesign@gmail.com>
2020-09-03 21:03:17 -0300

    multiplo de 5

---
+++ multiplo_5.dart
@@ -0,0 +1,11 @@
+/*Faça um programa em dart que, dado um número inteiro, indique se o número 
+é múltiplo de 5.
+date: 03/09/2020
+ */
+void main() {
+  int num = 255;
+  if (num % 5 == 0)
+    print("Múltiplo de 5");
+  else
+    print("Não Múltiplo de 5");
+}
